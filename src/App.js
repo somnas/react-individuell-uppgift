f4c0e7e4df5cad57017536f4e558b08ddb016f4f
somnas <erik.somnas@gmail.com>
2021-01-10 15:40:34 +0100

    add login form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-
+import React from 'react'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+      <h1>LOGIN</h1>      
+      <form>
+        <label>Email</label>
+        <input/>
+        <label>Password</label>
+        <input/>
+      </form>
     </div>
   );
 }
