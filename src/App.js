8391a13d8904f10d604f4169b433fdc9b45f1d28
somnas <erik.somnas@gmail.com>
2021-01-10 17:00:57 +0100

    add useState to login form

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+  
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  })
+
+  function handleOnChange(e) {
+    setFormData({...formData, [e.target.name]: e.target.value})
+    
+  }
+  
   return (
     <div className="App">
       <h1>LOGIN</h1>      
       <form>
         <label>Email</label>
-        <input/>
+        <input name="email" onChange={handleOnChange}/>
         <label>Password</label>
-        <input/>
+        <input name="password" onChange={handleOnChange}/>
+        <button type="submit">Login</button>
       </form>
     </div>
   );
